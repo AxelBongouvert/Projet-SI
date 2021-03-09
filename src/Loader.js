6d50fb15e566cc2ee745b55250b26f73c1f7d966
QuentinAutret <autret.quentin@gmail.com>
2021-03-09 14:58:25 +0100

    .

--- src/Loader.js
+++ src/Loader.js
@@ -8,7 +8,7 @@ class Loader extends React.Component {
     
     render() {
         return (
-            <a href="/Accueil">
+            <a href="/Choix">
                 <header class="masthead text-white text-center">
                     <div class="container d-flex align-items-center flex-column">
                         <img class="masthead-avatar mb-5" src="assets/img/dana.png" alt="" />
