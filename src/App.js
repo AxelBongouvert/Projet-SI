702c519afd1289b234fe898f3abce811f3290e32
QuentinAutret <autret.quentin@gmail.com>
2021-03-09 09:40:41 +0100

    init dana

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {
+  BrowserRouter,
+  Link,
+  Route,
+  Switch
+} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Demo from './Demo.js';
 
-export default App;
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    return (
+      <div>
+        <BrowserRouter basename={process.env.REACT_APP_ROUTER_BASE || ''}>
+          <div>
+            <Switch>
+              <Route path="/" component={Demo} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </div>
+      );
+    }
+  }
+  
+  export default Demo;
