7189d83647ab124c72624a526d72b7026a5e6a78
QuentinAutret <autret.quentin@gmail.com>
2021-03-09 15:37:24 +0100

    modif accueil

--- src/Accueil.js
+++ src/Accueil.js
@@ -10,18 +10,6 @@ class Accueil extends React.Component {
     render() {
         return (
             <div>
-                <footer class="footer text-center">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-lg-4 mb-10 mb-lg-0">
-                               <a href=""><h4 class="text-uppercase mb-4">Je veux aider à déménager</h4></a>
-                            </div>
-                            <div class="col-lg-4 mb-10 mb-lg-0">
-                                <a href="/Questionnaire"><h4 class="text-uppercase mb-4">Je déménage</h4></a>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
                 <Menu></Menu>
             </div>
             );
