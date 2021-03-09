c98e090ef8f05e7748639a093e2266ed38bf1e81
QuentinAutret <autret.quentin@gmail.com>
2021-03-09 14:59:34 +0100

    ajout questionnaire

---
+++ src/Questionnaire.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Questionnaire extends React.Component {
+    
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return (
+            <div>
+                <p>Questionnaire</p>
+            </div>
+            );
+        }
+    }
+    
+    export default Questionnaire;
