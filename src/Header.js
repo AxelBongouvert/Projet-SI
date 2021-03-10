3eb8ea3780986b3ab7b82c867d7defce058b056f
QuentinAutret <autret.quentin@gmail.com>
2021-03-10 15:39:26 +0100

    réparation header

--- src/Header.js
+++ src/Header.js
@@ -9,7 +9,7 @@ class Header extends React.Component {
     render() {
         return (
             <div>
-                <nav class="navbar navbar-expand navbar-light bg-white topbar static-top shadow fixed-top">
+                <nav class="navbar navbar-expand navbar-light bg-white topbar static-top shadow">
                     <ul class="navbar-nav justify-content-center">
                         <h6 class="m-0 font-weight-bold text-primary">Tableau de bord</h6>
                     </ul>
