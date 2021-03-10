f0587ec4a55d0f2166233af6570f00207cb667b9
QuentinAutret <autret.quentin@gmail.com>
2021-03-10 09:10:31 +0100

    style page connexion.js

--- src/Connexion.js
+++ src/Connexion.js
@@ -22,27 +22,27 @@ class Loader extends React.Component {
     
     render() {
         return (            
-            <div class="row">
+            <div class="row text-center text-center align-items-center row h-100">
                 <div class="col-lg-8 mx-auto">
                     <form id="contactForm" name="sentMessage" novalidate="novalidate" onSubmit={this.handleSubmit}>
                         <div class="control-group">
-                            <label>Mot de passe</label>
+                            <label>Nom de compte</label>
                             <div class="form-group floating-label-form-group controls mb-0 pb-2">
-                                <input class="form-control" id="pseudo" type="text" placeholder="..." required="required" data-validation-required-message="Veuillez saisir votre nom de compte." onChange={this.handleInputChange}/>
+                                <input class="form-control text-center" id="pseudo" type="text" placeholder="..." required="required" data-validation-required-message="Veuillez saisir votre nom de compte." onChange={this.handleInputChange}/>
                                 <p class="help-block text-danger"></p>
                             </div>
                         </div>
                         <div class="control-group">
                             <label>Mot de passe</label>
                             <div class="form-group floating-label-form-group controls mb-0 pb-2">
-                                <input class="form-control" id="mdp" type="password" placeholder="..." required="required" data-validation-required-message="Veuillez saisir votre mot de passe." onChange={this.handleInputChange}/>
+                                <input class="form-control text-center" id="mdp" type="password" placeholder="..." required="required" data-validation-required-message="Veuillez saisir votre mot de passe." onChange={this.handleInputChange}/>
                                 <p class="help-block text-danger"></p>
                             </div>
                         </div>
                         <br />
                         <div id="success"></div>
                         <div class="form-group">
-                            <button class="btn btn-primary btn-xl" id="sendMessageButton" type="submit">Connexion</button>
+                            <button class="btn btn-primary btn-xl" id="submit" type="submit">Connexion</button>
                         </div>
                     </form>
                 </div>
