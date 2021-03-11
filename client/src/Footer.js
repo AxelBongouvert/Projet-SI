428a9e2780b58726d283d26985824bd680d42e1e
Axel BONGOUVERT <bongouvertaxel@gmail.com>
2021-03-11 15:03:12 +0100

    Ajout du bouton Creation demenagement

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -13,22 +13,27 @@ class Footer extends React.Component {
 				<nav class="navbar navbar-expand navbar-light bg-white topbar static-top shadow fixed-bottom">
 					<ul class="navbar-nav w-100 d-flex justify-content-around">
 						<li class="nav-item no-arrow">
-							<a class="nav-link" href="/Profil" id="profil" role="button" aria-haspopup="true" aria-expanded="false">
+							<a class="nav-link mt-3" href="/Profil" id="profil" role="button" aria-haspopup="true" aria-expanded="false">
 								<img class="img-profile rounded-circle" src="img/undraw_profile.svg"/>
 							</a>
 						</li>
 						<li class="nav-item no-arrow">
-							<a class="nav-link" href="/Promos" id="promos" role="button" aria-haspopup="true" aria-expanded="false">
+							<a class="nav-link mt-3" href="/Promos" id="promos" role="button" aria-haspopup="true" aria-expanded="false">
 								<i class="fas fa-cart-plus fa-fw"></i>
 							</a>
 						</li>
 						<li class="nav-item no-arrow">
-							<a class="nav-link" href="/Messages" id="messages" role="button" aria-haspopup="true" aria-expanded="false">
+							<button class="btn btn-danger btn-circle btn-lg mb-5" href="#" id="creation_demenagement">
+								<i class="fas fa-truck fa-fw"></i>
+							</button>
+						</li>
+						<li class="nav-item no-arrow">
+							<a class="nav-link mt-3" href="/Messages" id="messages" role="button" aria-haspopup="true" aria-expanded="false">
 								<i class="fas fa-envelope fa-fw"></i>
 							</a>
 						</li>
 						<li class="nav-item no-arrow">
-							<a class="nav-link" href="/Plus" id="plus" role="button" aria-haspopup="true" aria-expanded="false">
+							<a class="nav-link mt-3" href="/Plus" id="plus" role="button" aria-haspopup="true" aria-expanded="false">
 								<i class="fas fa-dot-circle fa-fw"></i>
 							</a>
 						</li>
