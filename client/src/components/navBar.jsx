e855d8ad66f3ec6cb60d0b9587bacf84e579f367
Axel BONGOUVERT <bongouvertaxel@gmail.com>
2021-03-16 14:18:48 +0100

    ...

--- client/src/components/navBar.jsx
+++ client/src/components/navBar.jsx
@@ -3,7 +3,6 @@ import React from "react";
 // Stateless Functional Component
 
 const NavBar = ({ totalCounters, totalVolume }) => {
-<<<<<<< HEAD
   return (
     <nav class="navbar navbar-expand navbar-light bg-danger topbar static-top shadow">
       <ul class="navbar-nav w-100 d-flex justify-content-around">
@@ -29,36 +28,6 @@ const NavBar = ({ totalCounters, totalVolume }) => {
       </ul>
     </nav>
   );
-=======
-	return (
-		<div>
-			<br /><br />
-			<nav className="navbar navbar-expand navbar-light bg-danger topbar static-top shadow fixed-bottom">
-				<ul class="navbar-nav w-100 d-flex justify-content-around">
-					<li class="nav-item no-arrow">
-						<span class="badge badge-pill badge-info m-4" style={{ width: 75 }}>
-							<li class="nav-item no-arrow">{totalCounters}</li>
-							<li class="nav-item no-arrow">Items</li>
-						</span>
-					</li>
-					<li class="nav-item no-arrow">
-						<span class="badge badge-pill badge-info m-4" style={{ width: 125 }}>
-							<li class="nav-item no-arrow">{totalVolume}</li>
-							<li class="nav-item no-arrow">Volume (m3)</li>
-						</span>
-					</li>
-					<li class="nav-item no-arrow">
-						<a href="/CheckCamions">
-							<button class="btn btn-success btn-circle mt-3">
-								<i class="fas fa-check"></i>
-							</button>
-						</a>
-					</li>
-				</ul>
-			</nav>
-		</div>
-	);
->>>>>>> 34fd818a93cea1b8fd4ab346f1d4bd396c900da6
 };
 
 export default NavBar;
